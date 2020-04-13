4c564a66f43d6d4db7b17f5c8771e16ef0c9087e
Calo Corp <marc.calo@codeimmersives.com>
2020-04-12 21:25:31 -0400

    i understand it now

--- main.js
+++ main.js
@@ -2,7 +2,48 @@
  * YOUR CODE HERE *
  ******************/
 
+const newPerson = function (firstName='Anonymous', lastName='Person', age, married=false){
 
+  return {
+
+    firstName,
+    lastName,
+    age,
+    married,
+    goingOn(){
+
+      return this.age +1 
+
+    },
+    ageUp(){
+
+      return this.age++
+    },
+
+    getFullName(){
+
+      return this.firstName + ' ' + this.lastName
+    },
+
+    marry(spouse){
+this.married =true
+spouse.married = true
+this.spouseName = spouse.getFullName()
+spouse.spouseName = this.getFullName()
+    },
+
+    divorce(exSpouse){
+
+      this.married = false
+      delete this.spouseName
+      exSpouse.married = false
+
+    delete exSpouse.spouseName
+    }
+  }
+
+  
+} 
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
